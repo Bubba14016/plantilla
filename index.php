2407a1db284ece66e263224ed54edbc1ca9c86e6
Bubba14016 <cartorres.95@gmail.com>
2017-08-18 16:23:26 -0600

    plantilla 1.0
    solo el login

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+            <meta content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" name="viewport">
+                <title>
+                    Iniciar Sesión
+                </title>
+                <link href="css/bootstrap.min.css" rel="stylesheet">
+                    <link href="css/materialice.min.css" rel="stylesheet">
+                    </link>
+                </link>
+                <link href="css/estilos.css" rel="stylesheet">
+                </link>
+            </meta>
+        </meta>
+    </head>
+    <body>
+        <div class="container login">
+        <form action="" method="post">
+            <div class="row">
+                <div class="col-md-3">
+                </div>
+                <div class="col-md-6">
+                    <div class="panel panel-default ">
+                        <div class="panel-heading text-center">
+                            <h3>
+                                Inicio de Sesión
+                            </h3>
+                        </div>
+                        <div class="panel-body">
+                        	<div class="row">
+                        		<div class="col-md-3"><h4>Nombre:</h4></div>
+                        		<div class="col-md-9"><input type="text" name="nombre" id="nombre" class="form-control" autofocus/></div>
+                        	</div>
+                        	<div class="row">
+                        		<div class="col-md-3"><h4>Contraseña:</h4></div>
+                        		<div class="col-md-9"><input type="password" name="clave" id="clave" class="form-control" /></div>
+                        	</div>
+                        </div>
+                        <div class="panel-footer">
+                        <div class="row">
+                        	<div class="col-md-4"><button type="button" class="form-control btn btn-primary">Ingresar</button></div>
+                        	<div class="col-md-4"><button type="reset" class="form-control btn btn-danger">Cancelar</button></div>
+                        	<div class="col-md-4"><button type="button" class="form-control btn btn-success">Registrarse</button></div>
+                        
+                    </div>
+                </div>
+                <div class="col-md-3">
+                </div>
+            </div>
+            </form>
+        </div>
+        <script crossorigin="a
+	nonymous" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" src="https://code.jquery.com/jquery-2.2.4.min.js">
+        </script>
+        <script src="js/bootstrap.min.js">
+        </script>
+        <script src="js/materialice.min.js">
+        </script>
+    </body>
+</html>
